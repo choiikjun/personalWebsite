0db0eb294e1d6cc699930f163fa2fb7ea6f168da
Matthew Choi <choiikjun@gmail.com>
2016-06-26 23:48:28 +1000

    first go

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('document').ready(function(){
+
+	//Modal window
+	//When you click on the image, the modal window pops up
+	$('.first-image').on('click', function(){
+		window.open('http://www.google.com/');
+	});
+
+	$('').
+
+
+});
+
